e39944520f032729cc61f9e7a68f19bfb5d21b3f
unknown <ajithkumarjm@gmail.com>
2018-09-06 00:51:48 +0530

    'finalTaskInitialCommit'

--- src/components/app.js
+++ src/components/app.js
@@ -1,55 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "babel-polyfill";
-import Header from './header';
-import Sidebar from './sidebar';
-import TaskDraggable from './taskDraggable';
-import Slideshow from './taskSlideshow';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { AnimatedSwitch } from 'react-router-transition';
+import Header from "./header";
+import Sidebar from "./sidebar";
+import TaskDraggable from "./taskDraggable";
+import Slideshow from "./taskSlideshow";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { AnimatedSwitch } from "react-router-transition";
+import FinalTask from "./finalTask";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
-
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <div id="wrapper" className="toggled deskWrap">
-                        <Sidebar />
-                        <div id="page-content-wrapper">
-                            <ReactCSSTransitionGroup
-                                transitionName="example"
-                                transitionAppear={true}
-                                transitionAppearTimeout={1000}
-                                transitionEnterTimeout={1000}
-                                transitionLeaveTimeout={1000}>
-                                <AnimatedSwitch
-                                    atEnter={{ opacity: 0 }}
-                                    atLeave={{ opacity: 2 }}
-                                    atActive={{ opacity: 1 }}
-                                    mapStyles={styles => {
-                                        if (styles.opacity > 1) {
-                                            return { display: 'none' }
-                                        }
-                                        return { opacity: styles.opacity }
-                                    }}
-                                >
-                                    <Route path="/" exact={true} component={Slideshow} />
-                                    <Route path="/task_draggable" component={TaskDraggable} />
-                                </AnimatedSwitch>
-                            </ReactCSSTransitionGroup>
-                        </div>
-                        {/* <Desk /> */}
-                    </div>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div id="wrapper" className="toggled deskWrap">
+            <Sidebar />
+            <div id="page-content-wrapper">
+              <ReactCSSTransitionGroup
+                transitionName="example"
+                transitionAppear={true}
+                transitionAppearTimeout={1000}
+                transitionEnterTimeout={1000}
+                transitionLeaveTimeout={1000}
+              >
+                <AnimatedSwitch
+                  atEnter={{ opacity: 0 }}
+                  atLeave={{ opacity: 2 }}
+                  atActive={{ opacity: 1 }}
+                  mapStyles={styles => {
+                    if (styles.opacity > 1) {
+                      return { display: "none" };
+                    }
+                    return { opacity: styles.opacity };
+                  }}
+                >
+                  <Route exact={true} path="/" component={FinalTask} />
+                  <Route path="/task_slideshow" component={Slideshow} />
+                  <Route path="/task_draggable" component={TaskDraggable} />
+                </AnimatedSwitch>
+              </ReactCSSTransitionGroup>
+            </div>
+            {/* <Desk /> */}
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
 class Sidebar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-    render() {
-        return (
-            <div id="sidebar-wrapper">
-                <div className="list-unstyled">
-                    <NavLink exact to="/" className="list-group-item list-group-item-action"><FontAwesomeIcon icon="clone" /> Task 1 <FontAwesomeIcon icon="caret-left" className="pointing" size="2x" /></NavLink>
-                    <NavLink exact to="/task_draggable" className="list-group-item list-group-item-action"><FontAwesomeIcon icon="hand-pointer" /> Task 2 <FontAwesomeIcon icon="caret-left" className="pointing" size="2x" /></NavLink>
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div id="sidebar-wrapper">
+        <div className="list-unstyled">
+          <NavLink
+            exact
+            to="/"
+            className="list-group-item list-group-item-action"
+          >
+            <FontAwesomeIcon icon="clone" /> Final Task{" "}
+            <FontAwesomeIcon icon="caret-left" className="pointing" size="2x" />
+          </NavLink>
+          <NavLink
+            to="/task_slideshow"
+            className="list-group-item list-group-item-action"
+          >
+            <FontAwesomeIcon icon="clone" /> Task 1{" "}
+            <FontAwesomeIcon icon="caret-left" className="pointing" size="2x" />
+          </NavLink>
+          <NavLink
+            to="/task_draggable"
+            className="list-group-item list-group-item-action"
+          >
+            <FontAwesomeIcon icon="hand-pointer" /> Task 2{" "}
+            <FontAwesomeIcon icon="caret-left" className="pointing" size="2x" />
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
